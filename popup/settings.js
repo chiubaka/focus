1f48d7cd4967300464d665e31e778c8f7a38395b
Daniel Chiu <daniel@chiubaka.com>
2017-11-28 13:38:32 -0800

    Add proof of concept storage for match patterns

--- popup/settings.js
+++ popup/settings.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
   $("#add-button").click(function() {
-    alert("Add!");
+    var newMatchPattern = $("#match-pattern-input").val();
+    saveMatchPattern(newMatchPattern).then(function() {
+      restoreMatchPatterns();
+
+      // var matchPatterns = $("#match-patterns");
+      // matchPatterns.append(match);
+      // matchPatterns.append($("<br/>"));
+    });
   });
+
+  restoreMatchPatterns();
 });
+
+function restoreMatchPatterns() {
+  getMatchPatterns().then(function(matchPatterns) {
+    $("#match-patterns").text(matchPatterns);
+  });
+}
+
+function getMatchPatterns() {
+  var storageItem = browser.storage.sync.get("matchPatterns");
+  return storageItem.then(function(res) {
+    return res.matchPatterns || [];
+  });
+}
+
+function saveMatchPattern(pattern) {
+  return getMatchPatterns().then(function(matchPatterns) {
+    matchPatterns.push(pattern);
+    return browser.storage.sync.set({
+      matchPatterns: matchPatterns
+    });
+  });
+}
